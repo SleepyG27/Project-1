80274b23b65a59135c2ece33112db93d54bc552f
SleepyG27 <93741481+SleepyG27@users.noreply.github.com>
2024-09-12 02:41:47 +0000

    gg

--- PointType.h
+++ PointType.h
@@ -1,35 +1,40 @@
 #pragma once
 #include <cmath>
 #include <iomanip>
+#include <iostream>
 using namespace std;
+
 template <class T>
-class PointType
-{
+class PointType {
 private:
-  T x;
-  T y;
+    T x;
+    T y;
 
 public:
-  //Default constructor 
-  PointType() {x = 0, y =0; {cout<< fixed << setprecision(1)
+    // Default constructor
+    PointType() : x(0), y(0) { }
 
-  //Constructor with parameters
-  PointType(T ux, T uy) : x(ux), y(uy) { ; }
+    // Constructor with parameters
+    PointType(T ux, T uy) : x(ux), y(uy) { }
 
-  // Setters
-  void setX(T ux) { x = ux; }
-  void setY(T uy) { y = uy; }
-  void SetPoint(T ux, T uy) { x = ux, y = uy; }
- 
-//Getters
-  T getX() {return x;}
-  T getY() {return y;}
-  void GetPoint(T &ux, T &uy) {ux = x, uy = y;}
+    // Setters
+    void setX(T ux) { x = ux; }
+    void setY(T uy) { y = uy; }
+    void setPoint(T ux, T uy) { x = ux; y = uy; }
 
-  double operator - (PointType<T> op2) {double distance = sqrt(pow(x-op2.x,2)+pow(y-op2.y,2));
-  return distance;  }
-void print ()
-{ cout << "( " << x << " ) " << y << endl;
-}
+    // Getters
+    T getX() const { return x; }
+    T getY() const { return y; }
+    void getPoint(T &ux, T &uy) const { ux = x; uy = y; }
 
+    // Overload subtraction operator to calculate the distance
+    double operator-(const PointType<T>& op2) const {
+        double distance = sqrt(pow(x - op2.x, 2) + pow(y - op2.y, 2));
+        return distance;
+    }
+
+    // Print the point
+    void print() const {
+        cout << "( " << x << " , " << y << " )" << endl;
+    }
 };
