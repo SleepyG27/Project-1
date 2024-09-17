060c929c8e91e4a0b8289a048cb2f5d11e55d4ee
SleepyG27 <93741481+SleepyG27@users.noreply.github.com>
2024-09-17 01:59:56 +0000

    new

--- main.cpp
+++ main.cpp
@@ -1,36 +1,18 @@
 #include <iostream>
 #include "PointType.h"
-#include <iomanip>
-using namespace std; 
+#include "CircleType.h"
+#include "CylinderType.h"
 
-//This is to test the main pointtype class
+int main() {
+    // Define a center point and a point on the circumference
+    PointType<double> center(0.0, 0.0);
+    PointType<double> circumference(3.0, 4.0);
 
+    // Create a cylinder with height
+    CylinderType<double> cylinder(10.0, center, circumference);
 
+    // Print the cylinder properties
+    cylinder.print();
 
-int main() 
-{cout << fixed << setprecision(1) << showpoint;
-{cout << "From default Constructor"<<endl;
-            PointType<int> p1I;
-            PointType< double> p1D;
-            cout << "Integer point:";
-            p1I.print();
-            cout << "Double point:";
-            p1D.print();
-            
-           cout << "\n Constructor with parameters"<<endl;
-            PointType<int> p2I(3, 5);
-            PointType< double> p2D(3.5, 5.5);
-            cout << "Integer point:";  
-            p2I.print();
-            cout << "Double point:";  
-            p2D.print();    
-            
-           cout<< "\n Distance calculation" << endl;
-            double distance = p1I - p2I;
-             cout << distance << endl;
-
-           cout<< "\n Distance calculation" << endl;
-          distance = p1D - p2D;
-           cout << distance << endl;
-
-           };
+    return 0;
+}
